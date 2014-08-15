0e76ac140a3170736651bfced5fb277cd54b0bf0
morrillo <gustavo.orrillo@moldeo.coop>
2014-08-15 13:21:19 +0000

    Modified version number

--- __openerp__.py
+++ __openerp__.py
@@ -20,7 +20,7 @@
 ##############################################################################
 {
     'name': 'Lote - Product Copy',
-    'version': '0.1',
+    'version': '0.1.1',
     'category': 'E-Lote',
     'description': "",
     'author': 'Moldeo Interactive',
